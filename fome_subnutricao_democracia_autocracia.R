849d5580caa9f84bed81a60ca2d0008d1fad250a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-11-02 22:59:16 -0300

    Add files via upload

---
+++ fome_subnutricao_democracia_autocracia.R
@@ -0,0 +1,25 @@
+
+# Fome e subnutrição em países democratas e autocratas -------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 02/11/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/hunger-and-undernourishment -----------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Ter uma dieta que é, ambos, suficiente em termos de requerimentos calóricos e diversa
+### para satisfazer os requisitos nutricionais adicionais é essencial para uma boa saúde.
+### Subnutrição, especialmente em crianças e mães, é um dos principais fatores de risco
+### para morte e outras consequências de má saúde.
+
+### As Nações Unidas tem estabelecido uma meta global como parte dos Objetivos do Desenvolvimento
+### Sustentável para acabar com a fome até 2030. Atualmente nós estamos distantes de alcançar
+### esse alvo.
+
+### Em nossa pesquisa sobre fome e subnutrição nós observamos em como muitas pessoas estão
+### subnutridas, onde elas vivem, subnutrição infantil, e insegurança alimentar em volta
+### do mundo.
+ 
+# -
+
+
+
